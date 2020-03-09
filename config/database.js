18a1a70f3154bcca2247463001bee6750981d9cd
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-03-09 08:48:42 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@
 // heroku host
 module.exports = {
     connection: {
-        host: 'eu-cdbr-west-02.cleardb.net',
-        user: 'b749f2f7762b0d',
-        password: 'fd26a1d5',
-        database: 'heroku_11a8db03044f776'
+        host: '',
+        user: '',
+        password: '',
+        database: ''
     }
 };
 

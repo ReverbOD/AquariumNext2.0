d24ddfb564cae79719e40d023db0d7e7aea62968
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-03-09 09:00:55 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,9 @@
 // create connection to database
-
-// heroku host
-module.exports = {
-    connection: {
-        host: '',
-        user: '',
-        password: '',
-        database: ''
-    }
-};
-
-// local host
-// module.exports = {
-//     host: 'localhost',
-//     user: 'Aquarium_admin',
-//     password: 'ZLJuXp9jdU2VZidB',
-//     database: 'aquarium_admin'
-// };
+W
+ //local host
+ module.exports = {
+     host: 'localhost',
+     user: 'Aquarium_admin',
+     password: 'ZLJuXp9jdU2VZidB',
+     database: 'aquarium_admin'
+ };
